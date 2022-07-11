5c012ab70e2b48ba0fc1076959ae7407c825481d
Nebojsa Gasparovic <nebojsa.gasparovic@bcf.uni-freiburg.de>
2022-07-11 16:14:49 +0200

    Start testing with pytest

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Manage usage of computational resources for different users, machine learning mo
 To run the app execute
 
 `uvicorn manager.main:app --host=0.0.0.0 --reload`
+
+# Testing
+
+To test the app run
+
+`pytest manager/database.py`
--- manager/database.py
+++ manager/database.py
@@ -2,10 +2,10 @@ from datetime import datetime
 import email
 import enum
 from click import echo
-from sqlalchemy import ForeignKey, create_engine
+from sqlalchemy import ForeignKey, create_engine, select
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String, Float, DateTime, Enum
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker, relationship, aliased
 from sqlalchemy_utils import URLType
 from sqlalchemy.orm import declarative_mixin
 from traitlets import default
@@ -77,13 +77,21 @@ session.add(linear_model)
 session.add_all([
     User(name='wendy', fullname='Wendy Williams', email='wendy@gmail', nickname='windy'),
     User(name='mary', fullname='Mary Contrary', email='mary@gmail', nickname='mary'),
-    User(name='fred', fullname='Fred Flintstone', email='fred@gmail', nickname='freddy')])
+    User(name='fred', fullname='Fred Flintstone', email='fred@gmail', nickname='freddy'),
+    Model(name='convolutional model', memorycost=2000, performancecost=9.0),
+    Model(name='recurrent', memorycost=10000, performancecost=20.1),
+    Model(name='feed-forward model', memorycost=1500, performancecost=15.3)])
 
 session.flush()
 
 first_simulation = Simulation(userid=ed_user.id, modelid=linear_model.id)
 
 session.add(first_simulation)
+session.add_all(
+    [Simulation(userid=1, modelid=2),
+    Simulation(userid=2, modelid=3),
+    Simulation(userid=1, modelid=1)]
+)
 
 
 
@@ -91,4 +99,14 @@ session.flush()
 session.commit()
 
 
+def test_basic_innerjoin():
 
+    usr = aliased(User)
+    sim = aliased(Simulation)
+    mod = aliased(Model)
+
+    joinquery = select(usr.fullname).join(sim, sim.userid==usr.id).distinct()
+
+    result = session.execute(joinquery).fetchone()
+
+    assert result[0] == 'Ed Jones'
